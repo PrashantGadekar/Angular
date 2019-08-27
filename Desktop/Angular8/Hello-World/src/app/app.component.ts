b1be11b8bc02d501c8405d613ab3449ebbe4e508
Prashant Ashok Gadekar <prashant.gadekar@cgi.com>
2019-08-27 09:38:21 +0530

    First Submission

---
+++ Desktop/Angular8/Hello-World/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<div> 
+           Hello Baccho!
+   </div>`,
+  styles: [`
+          div {
+            color : red;
+          }
+  `]
+})
+export class AppComponent {
+  title = 'Hello-World-Prashant';
+}
---
+++ Desktop/Angular8/binding/src/app/test/test.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+            <input [(ngModel)]="name" type="text">
+            {{name}}
+
+
+  `,
+  styles: [`
+          
+
+  `]
+})
+export class TestComponent implements OnInit {
+  public name ="";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick(){
+    console.log("Welcome to home");
+    
+  }
+
+  logMessage(value){
+    console.log(value);
+  }
+
+}
